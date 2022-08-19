d2902e2d17810fdb697a114c2fd19c6ca5780b24
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-08-19 23:10:46 +0300

    feature

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const ref = {
+    form : document.querySelector('.feedback-form'),
+    email : document.querySelector('.feedback-form input'),
+    textarea : document.querySelector('.feedback-form textarea'),
+}
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+ref.form.addEventListener('submit', onSubmitForm);
+ref.form.addEventListener('input', throttle(onFormData, 500));
+
+const formData = {};
+
+function onFormData(e) {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+    if (data) {
+        ref.email.value = data.email;
+        ref.textarea.value = data.message;
+    }
