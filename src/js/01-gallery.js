d3ac8f9ce29df8b72cd817845bc5f7571c547649
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-08-19 16:31:00 +0300

    feature

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach(element => {
+    // const galleryItem = document.createElement('li');
+    // galleryItem.className = 'gallery__item';
+
+    const galleryLink = document.createElement('a');
+    galleryLink.className = 'gallery__link';
+    galleryLink.href = element.original;
+
+    const galleryImage = document.createElement('img');
+    galleryImage.className = 'gallery__image';
+    galleryImage.src = element.preview;
+    galleryImage.alt = element.description;
+    galleryImage.setAttribute('title', element.description);
+
+    // galleryItem.append(galleryLink);
+
+    galleryLink.append(galleryImage);
+
+    items.push(galleryLink);
+});
+
+gallery.append(...items);
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+});
